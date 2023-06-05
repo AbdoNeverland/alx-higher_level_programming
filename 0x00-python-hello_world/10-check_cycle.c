f75c2159c71507d9332026e2273f0e932214959f
abdoneverland <ch.abdel.news@gmail.com>
2023-06-05 14:14:51 -0700

    task 10

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -10,11 +10,12 @@ int check_cycle(listint_t *list)
 
 	if (!list)
 		return (0);
-	do {
+	while (two && (two->next))
+	{
 		one = one->next;
 		two = two->next->next;
 		if (two == one)
 			return (1);
-	} while (!one && !two && !(two->next));
+	}
 	return (0);
 }
