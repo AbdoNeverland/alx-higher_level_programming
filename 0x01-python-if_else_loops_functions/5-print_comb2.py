9f483a09347bd64c04c999b6e8f0a8d1fe3df128
abdoneverland <ch.abdel.news@gmail.com>
2023-06-06 13:45:44 -0700

    task 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for i in range(0, 99, 1):
     print("{number:02d}, ".format(number=i), end="")
-print("{number:02d}".format(number=i))
+print("{number:02d}".format(number=i + 1))
