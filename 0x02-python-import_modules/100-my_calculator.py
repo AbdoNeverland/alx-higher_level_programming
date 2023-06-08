b0cceca6e3e6126a76a1bcaf281782d2d21ed161
abdoneverland <ch.abdel.news@gmail.com>
2023-06-08 22:28:59 +0100

    task 100

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import sys, calculator_1
+
+if __name__ == "__main__":
+    if len(sys.argv) < 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+    else:
+       a = int(sys.argv[1])
+       o = sys.argv[2]
+       b = int(sys.argv[3])
+       if (o == "+"):
+           print("{} + {} = {}".format(a, b, calculator_1.add(a, b)))
+       elif (o == "-"):
+           print("{} - {} = {}".format(a, b, calculator_1.sub(a, b)))
+       elif (o == "*"):
+           print("{} * {} = {}".format(a, b, calculator_1.mul(a, b)))
+       elif (o == "div"):
+           print("{} / {} = {}".format(a, b, calculator_1.div(a, b)))
+       else:
+           print("Unknown operator. Available operators: +, -, * and /")
+           sys.exit(1)
+       sys.exit(0)
