bd5da3f553745b50cde32841087c2a8644999785
abdoneverland <ch.abdel.news@gmail.com>
2023-06-10 09:31:50 +0100

    task 0

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
+    """prints integer
+
+    Args:
+        my_list: list
+
+    Returns:
+        The return none
+    """
     for i in my_list:
         print("{}".format(i))
