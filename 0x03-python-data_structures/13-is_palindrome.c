b5f7b9e81158d122b0793851f267189b4b1e96b4
abdoneverland <ch.abdel.news@gmail.com>
2023-06-13 00:13:29 +0100

    task 13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -61,34 +61,3 @@ int is_palindrome(listint_t **head)
 	reverse_linked_from_index(head, demi);
 	return (result);
 }
-/**
- * is_palindrome0 - checks if it's a palindrome
- *@head: the lis
- * Return: 0 if it is not a palindrome, 1 if it is a palindrome
- */
-int is_palindrome0(listint_t **head)
-{
-	int nb = 0, i, *v;
-	listint_t *h = *head;
-
-	for (nb = 0; h && h->next != NULL; h = h->next)
-		nb++;
-	if (nb == 0)
-		return (1);
-	v = malloc(nb * sizeof(int));
-	if (!v)
-		return (0);
-	for (h = *head, i = 0; h && h->next != NULL; h = h->next, i++)
-		v[i] = h->n;
-	v[i] = h->n;
-	for (i = 0; i <= nb / 2; i++)
-	{
-		if (v[i] != v[nb - i])
-		{
-			free(v);
-			return (0);
-		}
-	}
-	free(v);
-	return (1);
-}
