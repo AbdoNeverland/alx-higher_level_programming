ffcd43f7e8018037f6109c8866987f9fdd6f3c00
abdoneverland <ch.abdel.news@gmail.com>
2023-06-14 11:35:04 +0100

    task 7

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-def print_sorted_dictionary(a_dictionary):
-    for k, v in sorted(a_dictionary.items()):
-        print(f"{k}: {v}")
 def update_dictionary(a_dictionary, key, value):
-    d = a_dictionary.copy()
-    d[key] = value
-    return d
+    a_dictionary[key] = value
+    return a_dictionary
