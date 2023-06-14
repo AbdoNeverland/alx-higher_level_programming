8d516e572c4f04fde61b3cf63f640bc1be43687f
abdoneverland <ch.abdel.news@gmail.com>
2023-06-14 22:02:55 +0100

    task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,6 +2,8 @@
 def roman_to_int(roman_string):
     data = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100,
             "D": 500, "M": 1000}
+    if not roman_string:
+        return 0
     nl = list(map(lambda x: data[x], list(roman_string)))
     sum = i = 0
     while i < len(nl):
@@ -12,3 +14,4 @@ def roman_to_int(roman_string):
             sum += nl[i]
         i += 1
     return sum
+print(roman_to_int(None))
