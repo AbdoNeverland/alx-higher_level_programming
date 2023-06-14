57939df3860dac6e9f9b7bc146285b3b86486b0c
abdoneverland <ch.abdel.news@gmail.com>
2023-06-14 12:18:05 +0100

    task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary == None or len(a_dictionary) == 0:
+    if not a_dictionary or len(a_dictionary) == 0:
         return None
     top = [list(a_dictionary.keys())[0], list(a_dictionary.values())[0]]
     for k, v in a_dictionary.items():
