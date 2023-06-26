00f5dbf982b7ed4469c0cbc607bf533d99e28c5e
abdoneverland <ch.abdel.news@gmail.com>
2023-06-26 22:43:28 +0100

    task 3

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    d = None
+    s = ""
+    try:
+        d = a / b
+        s = "{:d}".format(d)
+    except (TypeError, ZeroDivisionError, ValueError):
+        return d
+    finally:
+        print(s)
