df903e51d522519367dac2c144f93b38bc19cfad
abdoneverland <ch.abdel.news@gmail.com>
2023-07-05 19:08:57 +0100

    task 101

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -5,13 +5,15 @@ b : int or float
 Return : a + b
 """
 
+
 def matrix_divided(matrix, div):
     '''a : int or float
     b : int or float
     Return: addition of a and b'''
+    
     e1 = "matrix must be a matrix (list of lists) of integers/floats"
     e2 = "Each row of the matrix must have the same size"
-    if not isinstance(div, (int,float)):
+    if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
@@ -21,7 +23,7 @@ def matrix_divided(matrix, div):
     for i, m in enumerate(matrix):
         if not isinstance(m, list):
             raise TypeError(e1)
-        if i != 0 and len(m) != len(matrix[i-1]):
+        if i != 0 and len(m) != len(matrix[i - 1]):
             raise TypeError(e2)
         new_matrix[i] = matrix[i].copy()
         for k, e in enumerate(m):
