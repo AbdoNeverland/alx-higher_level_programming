c05c7651e036874134de094169546053f2e1b90b
abdoneverland <ch.abdel.news@gmail.com>
2023-07-05 18:37:09 +0100

    task 101

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -3,11 +3,10 @@
 """
 import numpy
 
+
 def lazy_matrix_mul(m_a, m_b):
     """Function to find and return the max integer in a list of integers
         If the list is empty, the function returns None
     """
 
     return numpy.matmul(m_a, m_b)
-
-print(lazy_matrix_mul([[3, 4]], [[3, 4]]))
