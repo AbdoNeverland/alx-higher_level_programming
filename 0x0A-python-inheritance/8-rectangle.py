22607e8eb216d2247964fa9379ce7510654eb701
abdoneverland <ch.abdel.news@gmail.com>
2023-07-10 22:51:16 +0100

    task 8

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""module"""
+
+
+class BaseGeometry:
+    """ base geo"""
+    def area(self):
+        """raise"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """des"""
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+class Rectangle(BaseGeometry):
+    """class"""
+
+    def __init__(self, width, height):
+        """c"""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
