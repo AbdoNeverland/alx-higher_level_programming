885580552cd49860212bffefe3834abe5c138212
abdoneverland <ch.abdel.news@gmail.com>
2023-07-11 23:03:19 +0100

    task 11

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -24,6 +24,6 @@ class Student:
         return self.__dict__
 
     def reload_from_json(self, json):
-        """ fonction"""
+        """ fonction to load json"""
         for i, j in json.items():
             setattr(self, i, j)
