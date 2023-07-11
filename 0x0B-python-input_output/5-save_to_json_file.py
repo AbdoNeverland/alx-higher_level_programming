ce5457ee0cfc0dbe2c87395b8c71164d56140afb
abdoneverland <ch.abdel.news@gmail.com>
2023-07-11 22:19:41 +0100

    task 5

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -7,5 +7,5 @@ def save_to_json_file(my_obj, filename):
     """ function """
 
     with open(filename, "w", enconding="utf-8") as f:
-        f.write(json.dumps(my_obj))
+        json.dump(my_obj, f)
         f.close()
