fa641d7f0a9ba16c7f41b4af524c2094abd6def1
abdoneverland <ch.abdel.news@gmail.com>
2023-07-11 22:38:36 +0100

    task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,10 +5,9 @@ import sys
 
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
-d = sys.argv
 try:
     o = load_from_json_file("add_item.json")
 except Exception as e:
     o = []
-o.extend(d)
+o.extend(sys.argv[1:])
 save_to_json_file(o, "add_item.json")
